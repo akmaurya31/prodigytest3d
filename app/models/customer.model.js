2113b500cbde110f15f904a9c1f5ac51f4488bc1
ashbfc <74180087+ashbfc@users.noreply.github.com>
2020-11-20 10:19:21 +0530

    Update customer.model.js

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -238,4 +238,86 @@ Customer.getFatcamm_nov = (email, result) => {
   });
   
   };
+
+     //////////////////////////////////////////
+     Customer.change_bank = (email,bank_id, result) => {
+      let sqlquery="SELECT *,user_bank.id as bid FROM users INNER JOIN user_bank ON users.id=user_bank.user_id where users.email='"+`${email}`+"'";
+          
+      sql.query(sqlquery, (err, res) => {  
+      if (Array.isArray(res) && res.length) {
+
+              let my_user_id = res[0].user_id; 
+              let my_bank_id= bank_id;
+      console.log("Email found");
+      let sql_userbank = "update user_bank set isprimary_bank=0 where user_id="+`${my_user_id}`; 
+     
+      sql.query(sql_userbank, function (err, resvv) {
+
+        console.log("Bank Details updated to 0: ",my_user_id);
+       
+        sql_userbank = "update user_bank set isprimary_bank=1 where user_id="+`${my_user_id}`+" and id="+`${my_bank_id}`; 
+     
+      sql.query(sql_userbank, function (err, resvv) {
+        console.log("sql: ",sql_userbank);
+        if(resvv.affectedRows>0){
+         console.log("Bank Details updated: ",resvv);
+        result(null,{ status:200, message:"Bank Details updated "});
+        }
+        else{
+          console.log("Bank Details not updated, bankid not found : ",resvv);
+        result(null,{ status:400, message:"Bank Details not updated, bankid not found."});
+        }
+      });
+      });
+       
+          }
+            else{   
+              console.log("Email not found"); 
+             console.log(sqlquery)
+             result(null,{ status:400, message:"Email not found " }); 
+          }  
+        });
+        
+        };
+
+        //////////////////////////////////////////
+     Customer.delete_bank = (email,bank_id, result) => {
+      let sqlquery="SELECT *,user_bank.id as bid FROM users INNER JOIN user_bank ON users.id=user_bank.user_id where users.email='"+`${email}`+"'";
+          
+      sql.query(sqlquery, (err, res) => {  
+      if (Array.isArray(res) && res.length) {
+
+              let my_user_id = res[0].user_id; 
+              let my_bank_id= bank_id;
+      console.log("Email found");
+             
+        sql_userbank = "delete from user_bank where user_id="+`${my_user_id}`+" and id="+`${my_bank_id}`; 
+     
+      sql.query(sql_userbank, function (err, resvv) {
+        console.log("sql: ",sql_userbank);
+        if(resvv.affectedRows>0){
+         console.log("Bank Details deleted: ",resvv);
+         //console.log("created customer: ",resvv);
+         //result(null,{ status:200, message:"Bank Details added ",  data:resvv });
+        result(null,{ status:200, message:"Bank Details deleted "});
+        }
+        else{
+          console.log("Bank Details not deleted, bankid not found : ",resvv);
+        result(null,{ status:400, message:"Bank Details not deleted, bankid not found."});
+       }
+      });
+      
+       
+          }
+            else{   
+              console.log("Email not found"); 
+             console.log(sqlquery)
+             result(null,{ status:400, message:"Email not found"}); 
+          }  
+        });
+        
+        };
+
+
 module.exports = Customer;
+
