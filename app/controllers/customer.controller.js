e6cb83c7c388c2de8ab191c6e27f8a8b901d00c7
Avinash Maurya <akmaurya31@gmail.com>
2020-11-26 15:55:24 +0530

    Update customer.controller.js

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -1145,3 +1145,24 @@ exports.mandate = (req, res) => {
     });
   };
   
+exports.getUserDetails = (req, res) => {
+    //console.log(req.body)
+   // return;
+   let ash_data1=''
+   let errflag1=0
+   if(req.body.email===undefined || req.body.email==''){
+     ash_data1=" email: feild is required";
+     errflag1=1;
+     console.log(req.body.email);
+   }
+   if(errflag1===0){  
+    Customer.getUserDetails(req.body, (err, data)  => {
+     res.send(data);
+     return;     
+    });       
+     }else{    
+     console.log(" vvvvvvvvvvvvvv v111111111111111111");
+     res.json({ status:200, message:ash_data1,data11:"Validation Error" });
+     return;
+   };
+   };
